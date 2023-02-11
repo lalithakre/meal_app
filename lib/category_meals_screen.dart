8b2084bf6bfa0ecf894e02e4a20fee6fb7378a38
Lalit Thakre <lalit03thakre@gmail.com>
2023-02-12 00:37:56 +0530

    commit

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -9,16 +9,16 @@ class CategoryMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final routeArgs =
-        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
-    final categoryColor = routeArgs['color'];
+    // final categoryColor = routeArgs['color'];
     return Scaffold(
       appBar: AppBar(
         title: Align(alignment: Alignment.center, child: Text(categoryTitle!)),
-        backgroundColor: Colors.black,
+        // backgroundColor: Colors.black,
       ),
-      // backgroundColor: categoryColor,
+      // backgroundColor: categoryColor,/
       // body: ListView.builder(
       //   itemBuilder: (context, index) => {},
       //   itemCount: 10,
